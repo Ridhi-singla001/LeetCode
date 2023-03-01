395ce17a0d9ac4986d0cce5e4e5bb71a0c23b910
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-03-01 06:19:39 +0530

    Time: 2 ms (33.28%), Space: 39.4 MB (74.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,21 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
-        long s=0;
-        long e=x/2;
+        long start=0;
+        long end=x/2;
         long ans=0;
         if(x==1){
             return 1;
         }
-        while(s<=e){
-            long mid=(s+e)/2;
-            if(mid*mid==x){
-                return (int)mid;
-            }else if(mid*mid>x){
-                e=mid-1;
-            }else{
-                ans=mid;
-                s=mid+1;
-            }
+        while(start<=end){
+            long mid=(start+end)/2;
+                if(mid*mid==x){
+                    return (int)mid;
+                }else if(mid*mid>x){
+                    end=mid-1;
+                }else{
+                    ans=mid;
+                    start=mid+1;
+                }
         }
         return (int)ans;
     }
