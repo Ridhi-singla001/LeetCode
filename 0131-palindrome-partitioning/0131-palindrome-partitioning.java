047d300114806757d8b25bbe451eca564adda857
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-03-31 12:13:01 +0530

    Time: 10 ms (54.69%), Space: 54.8 MB (56.55%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,43 +1,29 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        // backtracking 
-        // 1. define `ans` to hold `candidates`
-        List<List<String>> ans = new ArrayList<>();
-        // 2. define `candidates` to hold all potential candidates (palindromic substring)
-        List<String> candidates = new ArrayList<>();
-        // 3. start backtrack from index 0
-        backtrack(s, ans, candidates, 0);
-        // 4. return ans
-        return ans;
+        List < List < String >> res = new ArrayList < > ();
+        List < String > path = new ArrayList < > ();
+        partitionHelper(0, s, path, res);
+        return res;
     }
-    
-    private void backtrack(String s, List<List<String>> ans, List<String> candidates, int start) {
-        // 1. check if the goal is fulfilled, 
-        // i.e. reaching the end of string in this problem
-        if (start == s.length()) {
-            ans.add(new ArrayList<>(candidates));
+    static void partitionHelper(int index, String s, List < String > path, List < List < String >> res) {
+        if (index == s.length()) {
+            res.add(new ArrayList < > (path));
             return;
         }
-        // 2. find all potential candidates
-        for (int i = start; i < s.length(); i++) {
-            // we want to test all substrings, each substring is a potential candidate
-            // e.g. "aab" -> "a", "a", "b", "ab", "aa", "b", "aab"
-            String candidate = s.substring(start, i + 1);
-            // 3. check if the current candidate is palindrome or not 
-            // if not, then we cannot push to `candidates`
-            if (!isPalindrome(candidate)) continue;
-            // 4. if so, push it to candidates
-            candidates.add(candidate);
-            // 5. backtrack with index + 1
-            backtrack(s, ans, candidates, i + 1);
-            // 6. remove the current substring from `candidates`
-            candidates.remove(candidates.size() - 1);
+        for (int i = index; i < s.length(); ++i) {
+            if (isPalindrome(s, index, i)) {
+                path.add(s.substring(index, i + 1));
+                partitionHelper(i + 1, s, path, res);
+                path.remove(path.size() - 1);
+            }
         }
     }
-    
-    // there are different ways to check if `s` is palindrome 
-    // here's one of the ways
-    private boolean isPalindrome(String s) {
-        return new StringBuilder(s).reverse().toString().equals(s);
+
+    static boolean isPalindrome(String s, int start, int end) {
+        while (start <= end) {
+            if (s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
     }
 }
