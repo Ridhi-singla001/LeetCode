fc6aeb19e3e6d18c3d0603203fb32714f49dd118
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-04-09 12:07:43 +0530

    Time: 1 ms (77.69%), Space: 42.2 MB (14.10%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -1,36 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new LinkedList();
-        if(root==null){
-            return ans;
-        }
-        Queue <TreeNode> q = new LinkedList();
-        q.add(root);
-        int level = 1;
-        while(!q.isEmpty()){
-            List<Integer> l = new ArrayList();
-            int c = q.size();
-            for(int i=0;i<c;i++){
-                TreeNode cur = q.poll();
-                l.add(cur.val);
-                if(cur.left!=null){
-                    q.add(cur.left);
-                }
-                if(cur.right!=null){
-                    q.add(cur.right);
-                }
+        List<List<Integer>> res=  new ArrayList<>();
+        if (root==null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean status=  true;
+
+        while (!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            for(int i = 0 ;i<n;i++){
+                TreeNode node = queue.poll();
+                temp.add(node.val);
+                if(node.left!=null) queue.add(node.left);
+                if(node.right!=null) queue.add(node.right);
             }
-            if(level%2==0){
-                List<Integer> ll = new ArrayList();
-                for(int i =l.size()-1;i>=0;i--){
-                    ll.add(l.get(i));
-                }
-                ans.add(ll);
-            }else{
-                ans.add(l);
+            if(!status){
+                Collections.reverse(temp);
             }
-            level++;
+            res.add(temp);
+            status = !status;
+
         }
-        return ans;
+        return res;
+
     }
 }
