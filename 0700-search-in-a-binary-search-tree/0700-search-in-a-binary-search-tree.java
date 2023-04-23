c365cb03604bf2e9c9ca54fdb3835e3dee7ed0c9
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-04-23 07:37:00 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (86.56%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -27,7 +27,7 @@
 // }
 class Solution {
 public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return root;
+        if(root == null) return null;
         if(root.val == val){
             return root;
         }
