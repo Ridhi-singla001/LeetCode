1649884cb59e60865501fcb511df68b145e3f6d0
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-06-20 13:17:44 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (84.80%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,29 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode list = new ListNode();
-        ListNode head = list;
-        while(list1!=null && list2!=null){
-            if(list1.val>list2.val){
-                list.next=list2;
+         ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
                 list2 = list2.next;
             }
-            else{
-                list.next = list1;
-                list1 = list1.next;
-            }
-            list = list.next;
+            curr = curr.next;
         }
-        while(list1!=null){
-            list.next = list1;
-            list1 = list1.next;
-            list = list.next;
+
+        if (list1 != null) {
+            curr.next = list1;
         }
-        while(list2!=null){
-            list.next = list2;
-            list2 = list2.next;
-            list = list.next;
+
+        if (list2 != null) {
+            curr.next = list2;
         }
-        list.next = null;
-        return head.next;
+
+        return dummy.next;
     }
 }
