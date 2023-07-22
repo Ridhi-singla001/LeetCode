5647ba949b33eb6e9ad9fda3c8147f04c299b688
Ridhi Singla <78259552+Ridhi-singla001@users.noreply.github.com>
2023-07-22 10:52:47 +0530

    Time: 6 ms (92.14%), Space: 41 MB (74.53%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,28 +1,31 @@
-class Solution {
-    int [][] direction =new int[][]{{2,1},{-2,1},{2,-1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
-   
-    public double knightProbability(int N, int K, int r, int c) {
-        double [][][] ways = new double[K+1][N][N];
-        ways[0][r][c]=1;
-        for(int k=1; k<=K;++k){
-            for(int i=0; i<N;++i){
-                for(int j=0; j<N;++j){
-                    for(int [] dir: direction){
-                        int oldR = i-dir[0];
-                        int oldC = j-dir[1];
-                        if(oldR>=0 && oldC>=0 && oldR<N && oldC<N){
-                            ways[k][i][j]+=(ways[k-1][oldR][oldC]/8.0);
-                        }
-                    }
-                }
+public class Solution {
+    double[][][] dp;
+    int[] xmove = {-2, -2, -1, -1, 1, 1, 2, 2};
+    int[] ymove = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+    public double knightProbability(int n, int k, int row, int col) {
+        dp = new double[n][n][k + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(dp[i][j], -1.0);
             }
         }
-        double res = 0;
-        for(int i=0; i<N;++i){
-            for(int j=0; j<N;++j){
-                res+=ways[K][i][j];
-            }
+        double favourableOutcome = solve(row, col, n, k);
+        double totalOutcome = Math.pow(8, k);
+        return favourableOutcome / totalOutcome;
+    }
+
+    private double solve(int row, int col, int n, int k) {
+        if (row < 0 || col < 0 || row >= n || col >= n) return 0;
+        if (k == 0) return 1;
+        if (dp[row][col][k] != -1.0) return dp[row][col][k];
+
+        double ans = 0;
+
+        for (int i = 0; i < 8; i++) {
+            ans += solve(row + xmove[i], col + ymove[i], n, k - 1);
         }
-        return res;
+
+        return dp[row][col][k] = ans;
     }
 }
